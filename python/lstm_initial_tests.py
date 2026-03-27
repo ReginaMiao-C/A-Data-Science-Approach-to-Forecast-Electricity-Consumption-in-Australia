9b646e9b134f26137c54b5461d815edc56644138
“Molly <z5574769@ad.unsw.edu.au>
2026-03-27 14:39:19 +1100

    Continued work on initial LSTM model to add multiple loss functions and experiment with preprocessing techniques

--- python/lstm_initial_tests.py
+++ python/lstm_initial_tests.py
@@ -5,11 +5,14 @@ import torch.nn as nn
 import sys
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 
 
-np.random.seed(0)
+torch.manual_seed(0)
 use_all_data = True
+year_month_day = False
 cyclical_encoding = True
+drop_orig_dates = False
 
 cwd = Path.cwd()
 root_folder = cwd.parent
@@ -18,21 +21,31 @@ data_folder = root_folder / 'data'
 df = pd.read_csv(data_folder / 'all_data.csv')
 df = df.rename(columns={'Unnamed: 0': 'date'})
 df['date'] = pd.to_datetime(df['date'])
-# pytorch tensors need numerical inputs
 df['year'] = df['date'].dt.year
-df['day'] = df['date'].dt.strftime('%j') #day of year
 
-#df['month'] = df['date'].dt.month
-#df['day'] = df['date'].dt.day
+if year_month_day:
+    df['day'] = df['date'].dt.strftime('%j') #day of year
+else: 
+    df['month'] = df['date'].dt.month
+    df['day'] = df['date'].dt.day
 df = df.drop(columns='date')
 df = df.astype('float32')
 
 # cyclical feature encoding
 # https://machinelearningmastery.com/7-pandas-tricks-for-time-series-feature-engineering/
 if cyclical_encoding: # TODO: confirm how to handle leap years
-    df['day_sin'] = np.where(df['year'].isin([2012.0, 2016.0, 2020.0]), np.sin(2 * np.pi * df['day'] / 366), np.sin(2 * np.pi * df['day'] / 365))
-    df['day_cos'] = np.where(df['year'].isin([2012.0, 2016.0, 2020.0]), np.cos(2 * np.pi * df['day'] / 366), np.cos(2 * np.pi * df['day'] / 365))
-    df = df.drop(columns='day')
+    if year_month_day:
+        df['day_sin'] = np.where(df['year'].isin([2012.0, 2016.0, 2020.0]), np.sin(2 * np.pi * df['day'] / 366), np.sin(2 * np.pi * df['day'] / 365))
+        df['day_cos'] = np.where(df['year'].isin([2012.0, 2016.0, 2020.0]), np.cos(2 * np.pi * df['day'] / 366), np.cos(2 * np.pi * df['day'] / 365))
+        if drop_orig_dates:
+            df = df.drop(columns='day')
+    else:
+        df['month_sin'] = np.sin(2 * np.pi * df['month'] / 12)
+        df['month_cos'] = np.cos(2 * np.pi * df['month'] / 12)
+        df['day_sin'] = np.where(df['month']==2.0, np.where(df['year'].isin([2012.0, 2016.0, 2020.0]), np.sin(2 * np.pi * df['day'] / 29), np.sin(2 * np.pi * df['day'] / 28)), np.where(df['month'].isin([4.0, 6.0, 9.0, 11.0]), np.sin(2 * np.pi * df['day'] / 30), np.sin(2 * np.pi * df['day'] / 31)))
+        if drop_orig_dates:
+            df = df.drop(columns=['day', 'month'])
+
 
 
 # split into training and testing
@@ -102,7 +115,9 @@ class LSTMmodel(nn.Module):
 
 # train model
 model = LSTMmodel(input_size=feature_vars, hidden_size=64, num_layers=2)
-criterion = nn.MSELoss()
+criterion_mse = nn.MSELoss() # penalise large errors more heavily
+criterion_mae = nn.L1Loss()
+# NOTE: could consider Huber loss as an alternative
 # TODO: add multiple loss functions (MAE as well)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 # TODO: consider other optimisers/learning rate
@@ -111,7 +126,10 @@ epochs = 50
 for e in range(epochs):
     model.train()
     output = model(x_train) # TODO: consider batching (dataloader)?
-    loss = criterion(output.squeeze(), y_train)
+    mse_loss = criterion_mse(output.squeeze(), y_train)
+    mae_loss = criterion_mae(output.squeeze(), y_train)
+    loss = 0.5 * mse_loss + 0.5 * mae_loss
+    # TODO: experiment with weights?
     optimizer.zero_grad() # reset gradients
     loss.backward() # computes loss gradients
     optimizer.step() # updates weights from gradients * lr
@@ -125,6 +143,10 @@ with torch.no_grad():
     y_pred_scaled = model(x_test).squeeze()
 
 y_pred = scaler_y.inverse_transform(y_pred_scaled.reshape(-1, 1))
-print(f'True value: {y_test}\nPredicted value: {y_pred}')
+
+print(f'True value: {round(y_test.item(), 3)}\nPredicted value: {round(y_pred.item(), 3)}\n')
+print(f'Test MAE: {round(mean_squared_error(y_test, y_pred), 3)}')
+print(f'Test MSE: {round(mean_absolute_error(y_test, y_pred), 3)}')
+print(f'Test MAPE: {round(mean_absolute_percentage_error(y_test, y_pred), 3)*100}%')
 
 

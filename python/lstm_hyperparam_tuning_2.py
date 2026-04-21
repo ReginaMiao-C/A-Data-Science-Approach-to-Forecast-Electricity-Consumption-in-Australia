43aa1c3cb6fabcc6a2dfb4e50d5df871d74fb6cb
“Molly <z5574769@ad.unsw.edu.au>
2026-04-21 14:39:23 +1000

    finished hyperparameter tuning

---
+++ python/lstm_hyperparam_tuning_2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pathlib import Path
+import torch
+import sys
+import numpy as np
+import public_holidays as ph
+import lstm_functions as lf
+
+
+torch.manual_seed(0)
+
+cwd = Path.cwd()
+root_folder = cwd.parent
+data_folder = root_folder / 'data'
+res_path = root_folder / 'Results' / 'LSTM' / 'Final' 
+
+df = pd.read_csv(data_folder / 'all_data_30min.csv')
+val_y_start_idx = 3408
+
+df = lf.preprocess_30_min_data(df, True)
+df, df_datetime, results = lf.eval_df(df)
+
+results = lf.repeat_windows(df, results, df_datetime, val_y_start_idx, round((len(df) - val_y_start_idx)/(48*20)), 20, retrain=True)
+lf.display_metrics(results, True, file_path=res_path, file_name='m2.csv')

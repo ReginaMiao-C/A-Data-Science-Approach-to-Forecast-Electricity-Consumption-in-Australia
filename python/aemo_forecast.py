bf7a663acccfb8d16462beaf63dc02b6947b9147
“Molly <z5574769@ad.unsw.edu.au>
2026-04-24 17:06:54 +1000

    produced additional formats of AEMO data

--- python/aemo_forecast.py
+++ python/aemo_forecast.py
@@ -6,7 +6,9 @@ cwd = Path.cwd()
 root_folder = cwd.parent
 data_folder = root_folder / 'data'
 # set source folder for data
-source_folder = Path(r'C:\Users\...')
+source_folder = Path(r'C:\Users\molly\OneDrive\Documents\UNSW\Project\Data')
+
+peak_only = False
 
 # read forecast demand data
 if (source_folder / 'forecastdemand_nsw.csv').exists():
@@ -33,15 +35,17 @@ df['date'] = df['datetime'].dt.date
 df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
 df = df[df['date']!='2010-01-01']
 
-# get corresponding data to daily peak demand
-max_idx = df.groupby('date')['total_demand'].idxmax()
-peak_df = df.loc[max_idx].reset_index(drop=True)
+if peak_only:
+    # get corresponding data to daily peak demand
+    max_idx = df.groupby('date')['total_demand'].idxmax()
+    df = df.loc[max_idx].reset_index(drop=True)
 
-# one to many merge on actual peak demand
-forecast = pd.merge(peak_df, forecast, on='datetime', how='left')
+# one to many merge on actual  demand
+forecast = pd.merge(df, forecast, on='datetime', how='left')
 forecast['forecast_datetime'] = pd.to_datetime(forecast['forecast_datetime'])
 forecast['forecast_date'] = forecast['forecast_datetime'].dt.date
 forecast['forecast_date'] = pd.to_datetime(forecast['forecast_date'], format='%Y-%m-%d')
+
 
 # get last forecast from previous day
 forecast['prev_date'] = forecast['date'] - pd.Timedelta(days=1)
@@ -49,5 +53,28 @@ forecast = forecast[forecast['prev_date'] == forecast['forecast_date']]
 forecast = forecast.groupby('datetime').tail(1).reset_index(drop=True)
 forecast = forecast.drop(columns=['prev_date', 'rainfall', 'pv_capacity', 'temperature', 'solar_power','time', 'date', 'forecast_date'])
 
-forecast.to_csv(data_folder / 'peak_forecasts.csv', index=False)
+if peak_only:
+    forecast.to_csv(data_folder / 'peak_forecasts.csv', index=False)
+else:
+    forecast.to_csv(data_folder / 'all_forecasts.csv', index=False)
+
+    
+    forecast['datetime'] = pd.to_datetime(forecast['datetime'])
+    forecast['date'] = forecast['datetime'].dt.date
+    forecast['time'] = forecast['datetime'].dt.time
+
+    true_max_idx = forecast.groupby('date')['total_demand'].idxmax()
+    pred_max_idx = forecast.groupby('date')['forecast_demand'].idxmax()
+    true_max = forecast.iloc[true_max_idx, 1]
+    pred_max = forecast.iloc[pred_max_idx, 2]
+    true_max_time = forecast.iloc[true_max_idx, 5]
+    pred_max_time = forecast.iloc[pred_max_idx, 5]
+
+    forecast_time = pd.DataFrame(columns=['date', 'true_peak', 'pred_peak', 'true_peak_time', 'pred_peak_time'])
+    forecast_time['date'] = forecast['date'].unique()
+    forecast_time['true_peak'] = true_max.values
+    forecast_time['pred_peak'] = pred_max.values
+    forecast_time['true_peak_time'] = true_max_time.values
+    forecast_time['pred_peak_time'] = pred_max_time.values
+    forecast_time.to_csv(data_folder / 'aemo_peak_and_time.csv', index=False)
 
